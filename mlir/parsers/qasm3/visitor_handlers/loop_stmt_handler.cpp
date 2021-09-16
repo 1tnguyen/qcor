8e99a0d9674f8c61d4b391924d8e64a62b944d40
Thien Nguyen <nguyentm@ornl.gov>
2021-09-16 13:14:15 -0400

    Fixing a bug in QPE: integer power within a while loop
    The yield op must be inserted at the exit block if created.
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- mlir/parsers/qasm3/visitor_handlers/loop_stmt_handler.cpp
+++ mlir/parsers/qasm3/visitor_handlers/loop_stmt_handler.cpp
@@ -621,8 +621,13 @@ void qasm3_visitor::createWhileLoop(
   // 'After' block must end with a yield op.
   {
     mlir::OpBuilder::InsertionGuard g(builder);
-    mlir::Operation &lastOp = whileOp.after().front().getOperations().back();
-    builder.setInsertionPointAfter(&lastOp);
+    if (auto b = symbol_table.get_last_created_block()) {
+      builder.setInsertionPointToEnd(b);
+      symbol_table.set_last_created_block(nullptr);
+    } else {
+      mlir::Operation &lastOp = whileOp.after().front().getOperations().back();
+      builder.setInsertionPointAfter(&lastOp);
+    }
     builder.create<mlir::scf::YieldOp>(location);
   }
 
