e28ce2c41c8021f5c4faea7275cbd3be5ac5f3a0
Thien Nguyen <nguyentm@ornl.gov>
2021-10-06 12:39:04 -0400

    Added lowering of region-based modifier block
    Simple pattern: inject start and end qrt calls, inlining the modifier region in the middle.
    
    Reuse the lowering code for start and end.
    
    Signed-off-by: Thien Nguyen <nguyentm@ornl.gov>

--- mlir/transforms/quantum_to_llvm.cpp
+++ mlir/transforms/quantum_to_llvm.cpp
@@ -24,6 +24,7 @@
 #include "lowering/CallableLowering.hpp"
 #include "lowering/ComputeMarkerLowering.hpp"
 #include "lowering/ConditionalOpLowering.hpp"
+#include "lowering/ModifierRegionLowering.hpp"
 
 namespace qcor {
 mlir::Type get_quantum_type(std::string type, mlir::MLIRContext *context) {
@@ -111,14 +112,18 @@ void QuantumToLLVMLoweringPass::runOnOperation() {
   patterns.insert<AssignQubitOpConversion>(&getContext(), variables);
   patterns.insert<QarraySliceOpLowering>(&getContext(), variables);
   patterns.insert<QarrayConcatOpLowering>(&getContext(), variables);
-  patterns.insert<StartPowURegionOpLowering>(&getContext());
-  patterns.insert<EndPowURegionOpLowering>(&getContext());
-  patterns.insert<StartAdjointURegionOpLowering>(&getContext());
-  patterns.insert<EndAdjointURegionOpLowering>(&getContext());
+  // patterns.insert<StartPowURegionOpLowering>(&getContext());
+  // patterns.insert<EndPowURegionOpLowering>(&getContext());
+  // patterns.insert<StartAdjointURegionOpLowering>(&getContext());
+  // patterns.insert<EndAdjointURegionOpLowering>(&getContext());
+  patterns.insert<AdjURegionOpLowering>(&getContext());
+  patterns.insert<PowURegionOpLowering>(&getContext());
+  patterns.insert<CtrlURegionOpLowering>(&getContext());
+  patterns.insert<EndModifierRegionOpLowering>(&getContext());
   patterns.insert<ComputeMarkerOpLowering>(&getContext());
   patterns.insert<ComputeUnMarkerOpLowering>(&getContext());
-  patterns.insert<StartCtrlURegionOpLowering>(&getContext());
-  patterns.insert<EndCtrlURegionOpLowering>(&getContext());
+  // patterns.insert<StartCtrlURegionOpLowering>(&getContext());
+  // patterns.insert<EndCtrlURegionOpLowering>(&getContext());
   patterns.insert<TupleUnpackOpLowering>(&getContext());
   patterns.insert<CreateCallableOpLowering>(&getContext());
   patterns.insert<ConditionalOpLowering>(&getContext());
